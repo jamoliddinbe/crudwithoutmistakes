48571e4c73966def0e3ab35b1de25f631394f402
jamoliddinbe <bosuvay1@gmail.com>
2022-12-10 13:41:10 +0500

    first commit

---
+++ precache-manifest.9a7134eff555d4885fe27fa350aa6ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd04e1b5da5195ce58f9bddf965f2d6",
+    "url": "/react-hooks/index.html"
+  },
+  {
+    "revision": "31802ac4c86dcd04a091",
+    "url": "/react-hooks/static/css/main.8a15d8e6.chunk.css"
+  },
+  {
+    "revision": "1d7a4b1e284a66df3c0a",
+    "url": "/react-hooks/static/js/2.5dece2c9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-hooks/static/js/2.5dece2c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31802ac4c86dcd04a091",
+    "url": "/react-hooks/static/js/main.65cf955d.chunk.js"
+  },
+  {
+    "revision": "f02c9b9e9de0f8001e30",
+    "url": "/react-hooks/static/js/runtime-main.84c3037c.js"
+  }
+]);
